1f7324f82beb58a3defbad281f1e5fe0e4ae14a8
David Wang <wangd7@bxscience.edu>
2024-05-16 18:38:28 -0400

    fix: Remove testing config

--- config.ts
+++ config.ts
@@ -1,21 +1,21 @@
 export const config: { subway: Array<StopConfig>; bus: Array<StopConfig> } = {
   subway: [
     {
-      stop_ids: ["624S"],
+      stop_ids: ["405S"],
       walk_time: 10,
     },
     {
-      stop_ids: ["Q05S"],
+      stop_ids: ["D03S"],
       walk_time: 14,
     },
   ],
   bus: [
     {
-      stop_ids: ["401749", "803182"],
+      stop_ids: ["100017", "103400"],
       walk_time: 3,
     },
     {
-      stop_ids: ["403632"],
+      stop_ids: ["100723"],
       walk_time: 3,
     },
   ],
