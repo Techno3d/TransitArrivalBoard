0ab73adf7754deae8d362f575be749431d645350
Shadman Syed <shadman3d@gmail.com>
2025-04-17 22:42:46 -0400

    changed siri structs because certain fields could be left blank by the MTA

--- src/siri_structs.rs
+++ src/siri_structs.rs
@@ -15,7 +15,7 @@ pub struct Siri {
 #[derive(Serialize, Deserialize)]
 #[serde(rename_all = "PascalCase")]
 pub struct ServiceDelivery {
-  pub response_timestamp: String,
+  pub response_timestamp: Option<String>,
   pub stop_monitoring_delivery: Vec<StopMonitoringDelivery>,
   pub situation_exchange_delivery: Option<Vec<SituationExchangeDelivery>>,
 }
@@ -39,20 +39,20 @@ pub struct MonitoredStopVisit {
 #[serde(rename_all = "PascalCase")]
 pub struct MonitoredVehicleJourney {
   pub line_ref: String,
-  pub framed_vehicle_journey_ref: FramedVehicleJourneyRef,
-  pub journey_pattern_ref: String,
+  pub framed_vehicle_journey_ref: Option<FramedVehicleJourneyRef>,
+  pub journey_pattern_ref: Option<String>,
   pub published_line_name: Vec<String>,
-  pub operator_ref: String,
-  pub origin_ref: String,
+  pub operator_ref: Option<String>,
+  pub origin_ref: Option<String>,
   pub destination_ref: String,
   pub destination_name: Vec<String>,
   pub situation_ref: Option<Vec<SituationRef>>,
-  pub monitored: bool,
-  pub vehicle_location: Location,
-  pub bearing: f64,
-  pub progress_rate: String,
+  pub monitored: Option<bool>,
+  pub vehicle_location: Option<Location>,
+  pub bearing: Option<f64>,
+  pub progress_rate: Option<String>,
   pub block_ref: Option<String>,
-  pub vehicle_ref: String,
+  pub vehicle_ref: Option<String>,
   pub monitored_call: MonitoredCall,
 }
 
@@ -66,14 +66,14 @@ pub struct FramedVehicleJourneyRef {
 #[derive(Serialize, Deserialize)]
 #[serde(rename_all = "PascalCase")]
 pub struct SituationRef {
-  pub situation_simple_ref: String,
+  pub situation_simple_ref: Option<String>,
 }
 
 #[derive(Serialize, Deserialize)]
 #[serde(rename_all = "PascalCase")]
 pub struct Location {
-  pub longitude: f64,
-  pub latitude: f64,
+  pub longitude: Option<f64>,
+  pub latitude: Option<f64>,
 }
 
 #[derive(Serialize, Deserialize)]
@@ -83,23 +83,23 @@ pub struct MonitoredCall {
   pub expected_arrival_time: Option<String>,
   pub arrival_proximity_text: Option<String>,
   pub expected_departure_time: Option<String>,
-  pub distance_from_stop: i32,
-  pub number_of_stops_away: i32,
-  pub stop_point_ref: String,
-  pub visit_number: i32,
+  pub distance_from_stop: Option<i32>,
+  pub number_of_stops_away: Option<i32>,
+  pub stop_point_ref: Option<String>,
+  pub visit_number: Option<i32>,
   pub stop_point_name: Vec<String>,
 }
 
 #[derive(Serialize, Deserialize)]
 #[serde(rename_all = "PascalCase")]
 pub struct SituationExchangeDelivery {
-  pub situations: Situations,
+  pub situations: Option<Situations>,
 }
 
 #[derive(Serialize, Deserialize)]
 #[serde(rename_all = "PascalCase")]
 pub struct Situations {
-  pub pt_situation_element: Vec<PtSituationElement>,
+  pub pt_situation_element: Option<Vec<PtSituationElement>>,
 }
 
 #[derive(Serialize, Deserialize)]
@@ -107,14 +107,14 @@ pub struct Situations {
 pub struct PtSituationElement {
   pub publication_window: Option<PublicationWindow>,
   pub severity: Option<String>,
-  pub summary: Vec<String>,
-  pub description: Vec<String>,
-  pub situation_number: String,
+  pub summary: Option<Vec<String>>,
+  pub description: Option<Vec<String>>,
+  pub situation_number: Option<String>,
 }
 
 #[derive(Serialize, Deserialize)]
 #[serde(rename_all = "PascalCase")]
 pub struct PublicationWindow {
-  pub start_time: String,
-  pub end_time: String,
+  pub start_time: Option<String>,
+  pub end_time: Option<String>,
 }
