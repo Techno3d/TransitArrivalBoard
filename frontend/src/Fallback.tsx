e3acfa97c43d2cd42e09bacf81d2326c68544132
David Wang <rexmagnusdavid@gmail.com>
2026-02-09 12:58:40 -0500

    fix: Use StatusBar on Fallback

--- frontend/src/Fallback.tsx
+++ frontend/src/Fallback.tsx
@@ -1,34 +1,7 @@
-import { useEffect, useState } from "react";
-import { Fragment } from "react/jsx-runtime";
 import config from "../../config.json";
+import { StatusBar } from "./components/StatusBar";
 
 export function Fallback() {
-  const [time, setTime] = useState<string>(
-    new Date().toLocaleString("en-US", {
-      hour12: true,
-      timeZone: "America/New_York",
-      timeStyle: "short",
-      dateStyle: "short",
-    }),
-  );
-
-  useEffect(() => {
-    const loop = setInterval(() => {
-      setTime(
-        new Date().toLocaleString("en-US", {
-          hour12: true,
-          timeZone: "America/New_York",
-          timeStyle: "short",
-          dateStyle: "short",
-        }),
-      );
-    }, 1000);
-
-    return () => {
-      clearInterval(loop);
-    };
-  }, []);
-
   return (
     <div className="flex h-full touch-none flex-col gap-2 overflow-hidden overscroll-none bg-black font-sans select-none">
       <div
@@ -42,45 +15,7 @@ export function Fallback() {
           </h1>
         </div>
       </div>
-      <div className="flex h-12 shrink-0 flex-row items-center overflow-hidden bg-black px-2 text-3xl font-semibold text-white">
-        <div className="flex-1 text-start">
-          <h1>
-            {"Status: "}
-            <span className="inline-flex items-baseline rounded-xl bg-red-600 px-2">{"Error"}</span>
-          </h1>
-        </div>
-        <div className="text-center">
-          <h1>
-            {"Made with ❤️ by "}
-
-            {config.maintainers.length > 0
-              ? config.maintainers.map((maintainer, index) => {
-                  return (
-                    <Fragment key={maintainer.name}>
-                      <span className="inline-flex items-baseline">
-                        {maintainer.github_id ? (
-                          <img
-                            src={`https://avatars.githubusercontent.com/u/${maintainer.github_id}`}
-                            className="mx-2 aspect-square h-9 self-center rounded-full"
-                          />
-                        ) : null}
-
-                        <span>{maintainer.name}</span>
-                      </span>
-
-                      {config.maintainers.length >= 3 && index <= config.maintainers.length - 3 ? ", " : ""}
-                      {config.maintainers.length >= 3 && index == config.maintainers.length - 2 ? ", and " : ""}
-                      {config.maintainers.length == 2 && index == config.maintainers.length - 2 ? " and " : ""}
-                    </Fragment>
-                  );
-                })
-              : "no one"}
-          </h1>
-        </div>
-        <div className="flex-1 text-end">
-          <h1>{time}</h1>
-        </div>
-      </div>
+      <StatusBar credits={config.credits}></StatusBar>
     </div>
   );
 }
